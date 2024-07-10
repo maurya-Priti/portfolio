4c6c5f545c474635def3f592fa90df77a1229c18
Priti Maurya <maurya.priti032001@gmail.com>
2024-07-10 12:14:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ hamburger.addEventListener("click", mobileMenu);
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+
     
 }
 const navLink = document.querySelectorAll(".navbar a");
@@ -60,23 +61,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-//navbar active 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-window.onscroll = () => {
-    sections.forEach(sec => {
+document.addEventListener("DOMContentLoaded", function() {
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+    
+    window.onscroll = () => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-};
+
+        sections.forEach(sec => {
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+                });
+
+                let activeLink = document.querySelector('header nav a[href*=' + id + ']');
+                if (activeLink) {
+                    activeLink.classList.add('active');
+                }
+            }
+        });
+    };
+});
+
 // Experience section horizontal scroll
 document.addEventListener("DOMContentLoaded", function () {
   const timeline = document.getElementById('timeline');
